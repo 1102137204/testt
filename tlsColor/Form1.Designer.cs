9a54ef67c36ee9da8b53890631d76a46d1c4fade
RPC <RPC@DESKTOP-L5BOH5N>
2016-06-23 11:00:32 +0800

    Add project files.

---
+++ tlsColor/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace tlsColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tlsColorRed = new System.Windows.Forms.ToolStripButton();
+            this.tlsColorGreen = new System.Windows.Forms.ToolStripButton();
+            this.tlsColorBlue = new System.Windows.Forms.ToolStripButton();
+            this.tlsColorPurple = new System.Windows.Forms.ToolStripButton();
+            this.我是分隔線 = new System.Windows.Forms.ToolStripSeparator();
+            this.敲鍵盤快 = new System.Windows.Forms.ToolStripTextBox();
+            this.tlsCopy = new System.Windows.Forms.ToolStripButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlsColorRed,
+            this.tlsColorGreen,
+            this.tlsColorBlue,
+            this.tlsColorPurple,
+            this.我是分隔線,
+            this.敲鍵盤快,
+            this.tlsCopy});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tlsColorRed
+            // 
+            this.tlsColorRed.BackColor = System.Drawing.Color.Maroon;
+            this.tlsColorRed.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.tlsColorRed.Image = ((System.Drawing.Image)(resources.GetObject("tlsColorRed.Image")));
+            this.tlsColorRed.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlsColorRed.Name = "tlsColorRed";
+            this.tlsColorRed.Size = new System.Drawing.Size(23, 22);
+            this.tlsColorRed.Text = "toolStripButton1";
+            this.tlsColorRed.ToolTipText = "虹虹";
+            this.tlsColorRed.Click += new System.EventHandler(this.tlsColorRed_Click);
+            // 
+            // tlsColorGreen
+            // 
+            this.tlsColorGreen.BackColor = System.Drawing.Color.DarkGreen;
+            this.tlsColorGreen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.tlsColorGreen.Image = ((System.Drawing.Image)(resources.GetObject("tlsColorGreen.Image")));
+            this.tlsColorGreen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlsColorGreen.Name = "tlsColorGreen";
+            this.tlsColorGreen.Size = new System.Drawing.Size(23, 22);
+            this.tlsColorGreen.Text = "toolStripButton2";
+            this.tlsColorGreen.ToolTipText = "律律";
+            this.tlsColorGreen.Click += new System.EventHandler(this.tlsColorGreen_Click);
+            // 
+            // tlsColorBlue
+            // 
+            this.tlsColorBlue.BackColor = System.Drawing.Color.DarkBlue;
+            this.tlsColorBlue.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.tlsColorBlue.Image = ((System.Drawing.Image)(resources.GetObject("tlsColorBlue.Image")));
+            this.tlsColorBlue.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlsColorBlue.Name = "tlsColorBlue";
+            this.tlsColorBlue.Size = new System.Drawing.Size(23, 22);
+            this.tlsColorBlue.Text = "toolStripButton3";
+            this.tlsColorBlue.ToolTipText = "嵐嵐";
+            this.tlsColorBlue.Click += new System.EventHandler(this.tlsColorBlue_Click);
+            // 
+            // tlsColorPurple
+            // 
+            this.tlsColorPurple.BackColor = System.Drawing.Color.Purple;
+            this.tlsColorPurple.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.tlsColorPurple.Image = ((System.Drawing.Image)(resources.GetObject("tlsColorPurple.Image")));
+            this.tlsColorPurple.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlsColorPurple.Name = "tlsColorPurple";
+            this.tlsColorPurple.Size = new System.Drawing.Size(23, 22);
+            this.tlsColorPurple.Text = "toolStripButton4";
+            this.tlsColorPurple.ToolTipText = "仔仔";
+            this.tlsColorPurple.Click += new System.EventHandler(this.tlsColorPurple_Click);
+            // 
+            // 我是分隔線
+            // 
+            this.我是分隔線.Name = "我是分隔線";
+            this.我是分隔線.Size = new System.Drawing.Size(6, 25);
+            // 
+            // 敲鍵盤快
+            // 
+            this.敲鍵盤快.Name = "敲鍵盤快";
+            this.敲鍵盤快.Size = new System.Drawing.Size(100, 25);
+            this.敲鍵盤快.ToolTipText = "敲";
+            // 
+            // tlsCopy
+            // 
+            this.tlsCopy.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.tlsCopy.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tlsCopy.Image = ((System.Drawing.Image)(resources.GetObject("tlsCopy.Image")));
+            this.tlsCopy.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tlsCopy.Name = "tlsCopy";
+            this.tlsCopy.Size = new System.Drawing.Size(35, 22);
+            this.tlsCopy.Text = "複製";
+            this.tlsCopy.ToolTipText = "複製";
+            this.tlsCopy.Click += new System.EventHandler(this.tlsCopy_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkGreen;
+            this.textBox1.Font = new System.Drawing.Font("DFPingJuW7-B5", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gold;
+            this.textBox1.Location = new System.Drawing.Point(12, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 39);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "顯示項目";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tlsColorRed;
+        private System.Windows.Forms.ToolStripButton tlsColorGreen;
+        private System.Windows.Forms.ToolStripButton tlsColorBlue;
+        private System.Windows.Forms.ToolStripButton tlsColorPurple;
+        private System.Windows.Forms.ToolStripSeparator 我是分隔線;
+        private System.Windows.Forms.ToolStripTextBox 敲鍵盤快;
+        private System.Windows.Forms.ToolStripButton tlsCopy;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ tlsColor/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tlsColor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string output = "";
+            foreach (ToolStripItem item in toolStrip1.Items)
+            {
+                output += item.ToolTipText + "\r\n";
+            }
+
+            MessageBox.Show(output);
+        }
+
+        private void tlsCopy_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = 敲鍵盤快.Text;
+        }
+
+        private void tlsColorRed_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.DarkRed;
+        }
+
+        private void tlsColorGreen_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.DarkGreen;
+        }
+
+        private void tlsColorBlue_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.DarkBlue;
+        }
+
+        private void tlsColorPurple_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Purple;
+        }
+    }
+}
